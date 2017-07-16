5087095af881985d77753440373fdc2b53d01d19
william-taylor <wi11berto@yahoo.co.uk>
2017-07-16 19:21:11 +0100

    email support

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,18 +1,20 @@
 
-import * as child_process from 'child_process';
+import * as emailValidator from 'email-validator';
+import * as childProcess from 'child_process';
 import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
-import * as program from 'commander';
+import * as commander from 'commander';
+import * as emailjs from 'emailjs';
 import * as express from 'express';
 import * as crypto from 'crypto';
 import * as vhost from 'vhost';
 import * as cors from 'cors';
 import * as fs from 'fs';
 
-program.version('0.0.1');
-program.option('-h, --help', 'Help information');
-program.option('-l, --logs', 'Display logs');
-program.parse(process.argv);
+commander.version('0.0.1');
+commander.option('-h, --help', 'Help information');
+commander.option('-l, --logs', 'Display logs');
+commander.parse(process.argv);
 
 const decrypt = (json: any) => {
     const parse = (key: string, text: string) => {
@@ -29,7 +31,13 @@ const decrypt = (json: any) => {
     return json;
 }
 
-const read = (fn: string) => {
+interface EdgarConfig {
+    credentials: Object[];
+    applications: Object[];
+    server: Object;
+}
+
+const read = (fn: string) : EdgarConfig => {
     const contents = fs.readFileSync(fn); 
     return JSON.parse(contents.toString());  
 }
@@ -37,6 +45,14 @@ const read = (fn: string) => {
 const config = read('../config/dev.json');
 const router = express();
 const port = 3000;
+
+const { username, password, host } = decrypt(config.credentials);
+const emailServer = emailjs.server.connect({ 
+    user: username, 
+    password: password, 
+    host: host, 
+    ssl: true 
+});
 
 router.use(bodyParser.json());
 router.use(compression());
@@ -48,6 +64,25 @@ router.get('/get-applications', (req, res) => {
 
 router.get('/get-server-info', (req, res) => {
     res.json(config.server);
+});
+
+router.post('/send', (req, res) => {
+    const { email, subject, message } = req.body;
+
+    if (emailValidator.validate(email) && subject && message) {
+        const msg = {
+            text: `${message} Email : ${email}`,
+            from: "wi11berto@yahoo.co.uk",
+            to: "wi11berto@yahoo.co.uk",
+            subject: subject
+        };
+
+        emailServer.send(msg);
+
+        res.json({ msg: "Your email has been sent.", sent: true });
+    } else {
+        res.json({ msg: "Invalid request", sent: false, body: req.body });
+    }
 });
 
 router.listen(port, () => {
