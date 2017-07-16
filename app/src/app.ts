1ebb2e22523245fb2950f85a98671cb4faa994c8
william-taylor <wi11berto@yahoo.co.uk>
2017-07-16 02:40:21 +0100

    adding basic commander example

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,5 +1,11 @@
 
+import * as program from 'commander';
 import * as express from 'express';
+
+program.version('0.0.1');
+program.option('-h, --help', 'Help information');
+program.option('-l, --logs', 'Display logs');
+program.parse(process.argv);
 
 const router = express();
 const port = 3000;
@@ -9,5 +15,7 @@ router.get('/', (req, res) => {
 });
 
 router.listen(port, () => {
-    console.log(`Edgar on port ${port}!`);
+    if(program.logs) {
+        console.log(`Edgar on port ${port}!`);
+    }
 });
